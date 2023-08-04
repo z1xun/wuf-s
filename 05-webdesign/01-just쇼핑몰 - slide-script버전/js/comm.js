252e58f6550dbfae7336e5122671e8d0223d0141
jiseonn97 <jiseonn97@naver.com>
2023-08-04 11:05:08 +0900

    슬라이드

--- 05-webdesign/01-just쇼핑몰 - slide-script버전/js/comm.js
+++ 05-webdesign/01-just쇼핑몰 - slide-script버전/js/comm.js
@@ -23,14 +23,35 @@ $(function () {
     });
     //슬라이드
     let now = 1;
+    const slideEls = $(".lst-slide");
+    let height = 100;
 
+    //3초 마다 실행
     setInterval(function () {
         slide();
     }, 3000);
 
+    //슬라이드 기능 만들기
     function slide() {
-        $(".lst-slide").animate({
-            top: 100 * now * -1 + "%",
-        });
+        //조건문
+        // NOW가 0 일경우 1번째 슬라이드
+        // NOW가 1 일경우 2번째 슬라이드
+        // NOW가 2 일경우 3번째 슬라이드
+        //만약 1, 2번째 슬라이드일 경우 다음 슬라이드로 이동
+        //거짓:(3번째슬라이드일 경우) 다시 첫번째 슬라이드로 이동
+        if (now < 3) {
+            //참일 경우
+            slideEls.animate({
+                top: height * now * -1 + "%",
+            });
+            //변수 재선언
+            now = now + 1;
+        } else {
+            //거짓 일 경우
+            slideEls.animate({
+                top: 0,
+            });
+            now = 1;
+        }
     }
 });
