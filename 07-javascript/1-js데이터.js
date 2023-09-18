0f3c45b370f9d902a56ac3e03eb2282be761e839
jiseonn97 <jiseonn97@naver.com>
2023-09-18 12:17:59 +0900

    연산자

---
+++ 07-javascript/1-js데이터.js
@@ -0,0 +1,23 @@
+const string1 = "hello";
+const string2 = "hello";
+const string3 = "바보";
+
+console.log(string1, string2);
+console.log(string3, string2);
+
+const number = 123;
+const negative = -123;
+const float = 3.14;
+console.log(number + 1);
+console.log(negative);
+console.log(float);
+
+let b = 1;
+
+console.log(b++);
+console.log(++b);
+console.log(b);
+
+console.log(!true);
+
+console.log(false || true);
