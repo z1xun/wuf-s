318c3932457fd89f80c915713bf061c40e4784c4
z1xun <jsjh5934@naver.com>
2023-10-04 18:18:36 +0900

    기초틀

---
+++ 06-site/onepage/dist/js/comm.js
@@ -0,0 +1,23 @@
+const headerEl = document.querySelector("#header");
+
+//페이지에 스크롤 이벤트 추가
+window.addEventListener(
+    "scroll",
+    _.throttle(function () {
+        console.log(window.scrollY);
+
+        if (window.scrollY > 200) {
+            //페이지 스크롤 위치가 200px보다 크면
+            gsap.to(headerEl, 0.6, {
+                opacity: 0,
+                display: "none",
+            });
+        } else {
+            //페이지 스크롤 위치가 200px보다 작으면
+            gsap.to(headerEl, 0.6, {
+                opacity: 1,
+                display: "block",
+            });
+        }
+    }, 300)
+);
